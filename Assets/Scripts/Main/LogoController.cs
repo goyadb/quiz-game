3522843550919f99d94fc59fb98b08e07f9f7c11
freddy9118 <freddy369@naver.com>
2025-02-12 12:55:50 +0900

    f

---
+++ Assets/Scripts/Main/LogoController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using dot
+
+public class LogoController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
