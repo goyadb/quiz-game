a15670c3cdd811146ce1f67e6df81389cfcb392f
Junil Ko <junil@mocapot.com>
2025-02-27 09:47:25 +0900

    전면 광고 구현

--- Assets/Scripts/Commons/AdmobAdsManager.cs
+++ Assets/Scripts/Commons/AdmobAdsManager.cs
@@ -10,11 +10,13 @@ public class AdmobAdsManager : Singleton<AdmobAdsManager>
 
 #if UNITY_ANDROID
     private string _bannerAdUnitId = "ca-app-pub-3940256099942544/6300978111";
+    private string _interstitialAdUnitId = "ca-app-pub-3940256099942544/1033173712\r\n\r\n";
 #elif UNITY_IOS
     private string _bannerAdUnitId = "ca-app-pub-3940256099942544/2934735716";
 #endif
 
     private BannerView _bannerView;
+    private InterstitialAd _interstitialAd;
 
     private void Start()
     {
@@ -22,6 +24,9 @@ public class AdmobAdsManager : Singleton<AdmobAdsManager>
         {
             // 배너광고 표시
             LoadBannerAd();
+
+            // 전면광고 로드
+            LoadInterstitialAd();
         });
     }
 
@@ -55,6 +60,7 @@ public class AdmobAdsManager : Singleton<AdmobAdsManager>
 
         var adRequest = new AdRequest();
         _bannerView.LoadAd(adRequest);
+        RegisterBannerAdsEventHandler();
     }
 
     private void RegisterBannerAdsEventHandler()
@@ -96,6 +102,96 @@ public class AdmobAdsManager : Singleton<AdmobAdsManager>
         _bannerView.OnAdFullScreenContentClosed += () =>
         {
             Debug.Log("Banner view full screen content closed.");
+        };
+    }
+
+    #endregion
+
+    #region Interstitial Ads
+
+    /// <summary>
+    /// 전면 광고 준비(로드) 메서드
+    /// </summary>
+    public void LoadInterstitialAd()
+    {
+        if (_interstitialAd != null)
+        {
+            _interstitialAd.Destroy();
+            _interstitialAd = null;
+        }
+
+        Debug.Log("Loading the interstitial ad.");
+
+        var adRequest = new AdRequest();
+
+        InterstitialAd.Load(_interstitialAdUnitId, adRequest, (InterstitialAd ad, LoadAdError error) =>
+        {
+            if (error != null || ad == null)
+            {
+                Debug.LogError("interstitial ad failed to load an ad " + "with error: " + error);
+                return;
+            }
+
+            Debug.Log("Interstitial ad loaded with response: " + ad.GetResponseInfo());
+            _interstitialAd = ad;
+
+            RegisterInterstitialAdsEventHandlers(_interstitialAd);
+        });
+    }
+
+    public void ShowInterstitialAd()
+    {
+        if (_interstitialAd != null && _interstitialAd.CanShowAd())
+        {
+            Debug.Log("Showing the interstitial ad.");
+            _interstitialAd.Show();
+        }
+        else
+        {
+            Debug.Log("Interstitial ad is not ready yet.");
+        }
+    }
+
+    private void RegisterInterstitialAdsEventHandlers(InterstitialAd interstitialAd)
+    {
+        // Raised when the ad is estimated to have earned money.
+        interstitialAd.OnAdPaid += (AdValue adValue) =>
+        {
+            Debug.Log(String.Format("Interstitial ad paid {0} {1}.",
+                adValue.Value,
+                adValue.CurrencyCode));
+        };
+        // Raised when an impression is recorded for an ad.
+        interstitialAd.OnAdImpressionRecorded += () =>
+        {
+            Debug.Log("Interstitial ad recorded an impression.");
+        };
+        // Raised when a click is recorded for an ad.
+        interstitialAd.OnAdClicked += () =>
+        {
+            Debug.Log("Interstitial ad was clicked.");
+        };
+        // Raised when an ad opened full screen content.
+        interstitialAd.OnAdFullScreenContentOpened += () =>
+        {
+            Debug.Log("Interstitial ad full screen content opened.");
+        };
+        // Raised when the ad closed full screen content.
+        interstitialAd.OnAdFullScreenContentClosed += () =>
+        {
+            Debug.Log("Interstitial ad full screen content closed.");
+
+            // 전면 광고 닫히면 다시 로드
+            LoadInterstitialAd();
+        };
+        // Raised when the ad failed to open full screen content.
+        interstitialAd.OnAdFullScreenContentFailed += (AdError error) =>
+        {
+            Debug.LogError("Interstitial ad failed to open full screen content " +
+                           "with error : " + error);
+
+            // 전면 광고 로드 실패시 다시 로드
+            LoadInterstitialAd();
         };
     }
 
